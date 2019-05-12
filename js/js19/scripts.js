d33ba8e392ff303b2bc1805023af283db5ac0e1c
mgawande <mgawande@paypal.com>
2019-05-12 13:46:14 -0700

    adding js19, webcam fun

--- js/js19/scripts.js
+++ js/js19/scripts.js
@@ -5,17 +5,9 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  navigator.mediaDevices.getUserMedia({ video: true, audtio: false})
     .then(localMediaStream => {
       console.log(localMediaStream);
-    
-//  DEPRECIATION : 
-//       The following has been depreceated by major browsers as of Chrome and Firefox.
-//       video.src = window.URL.createObjectURL(localMediaStream);
-//       Please refer to these:
-//       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-//       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
-      
       video.srcObject = localMediaStream;
       video.play();
     })
@@ -30,81 +22,80 @@ function paintToCanvas() {
   canvas.width = width;
   canvas.height = height;
 
-  return setInterval(() => {
+  setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
+    //take pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with them
-    // pixels = redEffect(pixels);
-
-    pixels = rgbSplit(pixels);
-    // ctx.globalAlpha = 0.8;
-
-    // pixels = greenScreen(pixels);
-    // put them back
-    ctx.putImageData(pixels, 0, 0);
+    //modify pixels
+    //pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+    pixels = greenScreen(pixels);
+    //put them back
+    ctx.putImageData(pixels, 0, 0); 
   }, 16);
 }
 
 function takePhoto() {
-  // played the sound
+  //played the sound
   snap.currentTime = 0;
   snap.play();
 
-  // take the data out of the canvas
+  //take the data out of canvas
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
   strip.insertBefore(link, strip.firstChild);
+
 }
 
 function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  for(let i=0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;  //green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
   }
   return pixels;
 }
 
 function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  for(let i=0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; //red
+    pixels.data[i + 100] = pixels.data[i + 1];  //green
+    pixels.data[i - 150] = pixels.data[i + 2];//blue
   }
   return pixels;
 }
 
 function greenScreen(pixels) {
   const levels = {};
-
   document.querySelectorAll('.rgb input').forEach((input) => {
     levels[input.name] = input.value;
   });
 
-  for (i = 0; i < pixels.data.length; i = i + 4) {
+
+  for(i = 0; i < pixels.data.length; i = i + 4) {
     red = pixels.data[i + 0];
     green = pixels.data[i + 1];
     blue = pixels.data[i + 2];
     alpha = pixels.data[i + 3];
-
-    if (red >= levels.rmin
+  
+    if(red >= levels.rmin
       && green >= levels.gmin
       && blue >= levels.bmin
       && red <= levels.rmax
       && green <= levels.gmax
       && blue <= levels.bmax) {
-      // take it out!
+      //take it out
       pixels.data[i + 3] = 0;
     }
   }
 
   return pixels;
 }
-
 getVideo();
+
 
 video.addEventListener('canplay', paintToCanvas);
